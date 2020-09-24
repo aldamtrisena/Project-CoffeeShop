14709d0ffaf34354c8132a0af2805ee85be0cdf8
aldamtrisena <aldam3sena@gmail.com>
2020-09-24 20:58:36 +0700

    aldam

--- controllers/transaksiProductUser.js
+++ controllers/transaksiProductUser.js
@@ -3,6 +3,7 @@ const {
     User
 } = require("../models/index")
 
+
 class TransaksiController {
 
     static inputSales(req, res) {
@@ -16,46 +17,28 @@ class TransaksiController {
     }
 
     static postSalesController(req, res) {
-        let qty = req.body.quantity
-        Product.findAll({
-                where: {
-                    id: req.body.idProduck
-                }
-            })
+        let qty = req.body.qty
+        Product.findByPk(req.body.item)
             .then(data => {
-
-                // res.send(req.body)
-                console.log(req.body.quantity)
-                let totalqty = 0
-                let taotalprice = 0
-                for (let i = 0; i < data.length; i++) {
-                    taotalprice += data[i].price
-                    for (let j = 0; j < qty.length; j++) {
-                        totalqty += Number(qty[j])
-                    }
-                }
-
-                // console.log(totalqty, taotalprice)
-                let bigTotal = taotalprice * totalqty
-                //    console.log(bigTotal)
                 // res.send(data)
+                let bigTotal = data.price * qty
+                console.log(bigTotal)
                 let newdata = JSON.stringify(data)
-                // let newqty = JSON.stringify(qty)
                 res.redirect(`/pay/${3}/${bigTotal}?input=${newdata}`)
             })
     }
 
     static checkoutPay(req, res) {
-        // res.send(req.query)
-        // res.send(req.query)
         let total = req.params.total
-        console.log()
         let data = JSON.parse(req.query.input)
-        console.log(data)
         res.render("checkout", {
             data,
             total
         })
+    }
+
+    static finist() {
+
     }
 }
 
